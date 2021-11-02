dfc74849afe75c0456cc516ee98482bf4765732e
Joel Rocha Carrasquilla <joroca@fp.insjoaquimmir.cat>
2021-11-02 19:38:49 +0100

    actualizado

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+ 
+return [
+   "driver"    => "mysql",
+   "host"      => "127.0.0.1",
+   "port"      => 3306,
+   "database"  => "testdb",
+   "user"      => "dbuser",
+   "password"  => "dbpass"
+];
+?>
+
