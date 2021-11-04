165856c53ba1a00d428f14ad8e434980b98f7004
Bruno Saldaña <brsase@fp.insjoaquimmir.cat>
2021-11-04 19:21:17 +0100

    falso

---
+++ library/Mail.php
@@ -0,0 +1,37 @@
+<?php
+namespace My;
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\Exception as PHPMailerException;
+class Mail {
+   // PHPMailer
+   private $_mailer;
+   /**
+    * Constructs mail
+    *
+    * @param string $subject
+    * @param string $body
+    * @param bool $isHTML (optional)
+    */
+   public function __construct(string $subject, string $body, bool $isHTML = false)
+   {
+       $this->_mailer = new PHPMailer();
+       // Setup SMTP server...
+       // Configure mail contact: from and reply...
+       // Set subject and body (HTML or not)...
+   }
+   /**
+    * Send mail to recipients
+    *
+    * @param array $to
+    * @param array $cc (optional)
+    * @param array $bcc (optional)
+    * @return bool
+    */
+   public function send(array $to, array $cc = [], array $bcc = []) : bool
+   {
+       // Add recipients (to, cc, bcc)...
+       // Send mail...
+       // Clear recipients...
+   }
+}
+?>
