a70292d4ab2b46edd548231f6593030ef86311ef
NaClecs <alexsr2002@gmail.com>
2022-02-03 16:41:45 +0100

    m9about-us

---
+++ laravel/storage/framework/views/24b41feb328a305b3541d7cd4075806ef0df1fd9.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap version -->
+<div class="alert alert-<?php echo e($type); ?> alert-dismissible fade show" role="alert">
+   <?php echo e($message); ?> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div><?php /**PATH /var/www/html/tarda/projecte/laravel/resources/views/flash-message.blade.php ENDPATH**/ ?>
