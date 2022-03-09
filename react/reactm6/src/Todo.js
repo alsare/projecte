64f4586dd6a157adb80a764d7edf9b79737e8c1e
brunosaldana <brsase@fp.insjoaquimmir.cat>
2022-03-09 19:09:19 +0100

    subido todo lo de react que tengo ekisde

---
+++ react/reactm6/src/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div>Header</div>
+            <div>Login</div>
+            <div>Chats</div>
+                <div>Missatge</div>
+                <div>Missatge</div>
+                <div>Missatge</div>
+                <div>Missatge</div>
+                <div>Missatge</div>
+            <div>footer</div>
+
+
+
+        </div>
+      
+
+    )
+}
